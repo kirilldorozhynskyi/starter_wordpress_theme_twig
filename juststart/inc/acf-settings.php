07173c95b791a4e50ad140c687c5d92beef4ec40
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2021-06-06 19:20:08 +0200

    [DEV] Global settings for web

--- juststart/inc/acf-settings.php
+++ juststart/inc/acf-settings.php
@@ -15,11 +15,11 @@ add_filter('acf/settings/save_json', 'jd_acf_json_save_point');
 
 function jd_acf_json_save_point($path)
 {
-    // update path
-    $path = get_stylesheet_directory() . '/acf_json';
+	// update path
+	$path = get_stylesheet_directory() . '/acf_json';
 
-    // return
-    return $path;
+	// return
+	return $path;
 }
 
 /**
@@ -30,14 +30,14 @@ add_filter('acf/settings/load_json', 'jd_acf_json_load_point');
 
 function jd_acf_json_load_point($paths)
 {
-    // remove original path (optional)
-    unset($paths[0]);
+	// remove original path (optional)
+	unset($paths[0]);
 
-    // append path
-    $paths[] = get_stylesheet_directory() . '/acf_json';
+	// append path
+	$paths[] = get_stylesheet_directory() . '/acf_json';
 
-    // return
-    return $paths;
+	// return
+	return $paths;
 }
 
 // Add acf page setings
@@ -45,20 +45,26 @@ add_action('acf/init', 'jd_acf_init');
 
 function jd_acf_init()
 {
-    if (function_exists('acf_add_options_page')) {
-        acf_add_options_page([
-            'page_title' => __('Theme General Settings', '_juststart'),
-            'menu_title' => __('Theme Settings', '_juststart'),
-            'menu_slug' => 'theme-general-settings',
-            'post_id' => 'options',
-            'capability' => 'edit_posts',
-            'redirect' => 'false',
-        ]);
-        acf_add_options_sub_page([
-            'page_title' => __('Default Settings', '_juststart'),
-            'menu_title' => __('Default', '_juststart'),
-            'menu_slug' => 'theme-default-settings',
-            'parent_slug' => 'theme-general-settings',
-        ]);
-    }
+	if (function_exists('acf_add_options_page')) {
+		acf_add_options_page([
+			'page_title' => __('Theme General Settings', '_juststart'),
+			'menu_title' => __('Theme Settings', '_juststart'),
+			'menu_slug' => 'theme-general-settings',
+			'post_id' => 'options',
+			'capability' => 'edit_posts',
+			'redirect' => 'false',
+		]);
+		acf_add_options_sub_page([
+			'page_title' => __('Default Settings', '_juststart'),
+			'menu_title' => __('Default', '_juststart'),
+			'menu_slug' => 'theme-default-settings',
+			'parent_slug' => 'theme-general-settings',
+		]);
+		acf_add_options_sub_page([
+			'page_title' => __('Footer Settings', '_juststart'),
+			'menu_title' => __('Footer', '_juststart'),
+			'menu_slug' => 'theme-footer-settings',
+			'parent_slug' => 'theme-general-settings',
+		]);
+	}
 }
