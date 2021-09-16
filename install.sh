907ab5edd75b7c113a7ba3c330b82888c6a70e05
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2021-09-16 22:01:36 +0200

    [DEV] Replace theme files

--- install.sh
+++ install.sh
@@ -5,13 +5,5 @@ echo What is your theme name?
 read APP_NAME
 echo Your theme name is: $APP_NAME
 
-echo What is your WordPress theme path? exemple user/wordpress/wp-content/theme
-read APP_PATH
-echo Your theme path is: $APP_PATH
-cp -r `dirname "$0"`/juststart  $APP_PATH
-cd $APP_PATH
-
-mv juststart $APP_NAME
-cd $APP_PATH/$APP_NAME
 grep -rli "_juststart" * | xargs -I@ sed -i "" "s/_juststart/$APP_NAME/g" @
 echo Your theme was install
