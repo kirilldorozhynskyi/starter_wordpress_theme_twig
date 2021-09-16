8ce5f8277143b2286ccf27e64dcf5c0cc27d4dce
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2021-09-16 22:24:52 +0200

    [DEV] Change install script

--- install.sh
+++ install.sh
@@ -5,5 +5,29 @@ echo What is your theme name?
 read APP_NAME
 echo Your theme name is: $APP_NAME
 
+#  String replace
 grep -rli "_juststart" * | xargs -I@ sed -i "" "s/_juststart/$APP_NAME/g" @
+
+#  Remove composer
+if [[ -f composer.json ]] && [[ -f composer.lock ]]
+then
+    rm composer.lock
+    rm composer.json
+fi
+
+#  Remove composer vedor
+if [[ -f vendor ]]
+then
+    rmdir vendor/
+fi
+
+if [[ -f install.sh ]]
+then
+    rm install.sh
+fi
+
+#  Change translate
+cd languages/
+mv _jdlang.pot $APP_NAME.pot
+
 echo Your theme was install
